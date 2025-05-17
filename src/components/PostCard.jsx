b07763cca917e58f55e6184f6d61bccacf31bc3c
Shahid Ansari <shahidansaribhodu@gmail.com>
2025-05-18 01:15:26 +0530

    Login component

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -6,7 +6,7 @@ const PostCard = ({$id,title,featuredImage}) => {
   return (
     <div>
       <Link to={`/post/${$id}`}>
-      <div className='w-full bg-gary-100 rounded-xl p-4'>
+      <div className='w-full bg-gray-100 rounded-xl p-4'>
         <div className='w-full justify-center mb-4'>
             <img src={databaseservice.getfilePreview(featuredImage)} alt={title}  className='rounded-xl'/>
         </div>
