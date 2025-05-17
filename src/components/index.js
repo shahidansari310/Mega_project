61b160c861bd88b2166027acf09bfb956f33c5dd
Shahid Ansari <shahidansaribhodu@gmail.com>
2025-05-18 01:37:01 +0530

    Signup component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from "./container/container";
 import Logo from "./Logo/Logo";
 import Logout from "./Header/Logout";
+import Button from "./Button";
+import Input from "./Input";
 
-export {Header,Footer,Container,Logo,Logout}
+export {Header,Footer,Container,Logo,Logout,Button,Input}
