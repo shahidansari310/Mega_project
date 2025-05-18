fc2e2334b4eecb4c7b6c96c40fa41768a739f471
Shahid Ansari <shahidansaribhodu@gmail.com>
2025-05-18 12:57:45 +0530

    created RTE and postform component

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,8 @@ import Logo from "./Logo/Logo";
 import Logout from "./Header/Logout";
 import Button from "./Button";
 import Input from "./Input";
+import { RTE } from "./RTE";
+import Select from "./Select"
+import Signup from "./Signup"
 
-export {Header,Footer,Container,Logo,Logout,Button,Input}
+export {RTE,Header,Footer,Signup,Select,Container,Logo,Logout,Button,Input}
